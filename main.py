c5580411fb86d12ca1e5b9122c57498bffa75d3b
TomsaJ <julian.tomsa@gmail.com>
2024-04-21 11:06:41 +0000

    while schleife für den richtigen pfad

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ from installwhisper import check_and_install_package
 from transferfile import copy_to_tmp_directory
 
 if __name__ == "__main__":
+    a = False
     check_and_install_package()
-    # Fordere den Benutzer auf, den Dateipfad einzugeben
-    file_path = input("Geben Sie den Pfad zur Datei ein: ")
-
-    # Überprüfe, ob der angegebene Pfad gültig ist
-    if os.path.isfile(file_path):
-        copy_to_tmp_directory(file_path)
-    else:
-        print("Ungültiger Dateipfad.")
+    while a == False: 
+        # Fordere den Benutzer auf, den Dateipfad einzugeben
+        file_path = input("Geben Sie den Pfad zur Datei ein: ")
+        # Überprüfe, ob der angegebene Pfad gültig ist
+        if os.path.isfile(file_path):
+            copy_to_tmp_directory(file_path)
+            a = True
+        else:
+            print("Ungültiger Dateipfad.")
     untertitel(file_path)
