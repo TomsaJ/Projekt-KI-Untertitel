6d90e2a0c95823301e85c7ec459bb52616da010e
TomsaJ <julian.tomsa@gmail.com>
2024-05-04 14:14:15 +0000

    finish installation

--- src/installation.py
+++ src/installation.py
@@ -2,14 +2,18 @@ import sys
 import os
 src_path = "/home/jutom001/KI/src"
 sys.path.append(src_path)
-from file import TempFileManager
+from file import TempFileManager as tmp
+from timer import Tim
 
 class Installation:
     @staticmethod
     def startup():
-        tmp = TempFileManager()
         # Assuming jsonfile() is defined outside of TempFileManager class
-        tmp.jsonfile(12)
+        paths = ['10min.mp4','15min.mp4', '20min.mp4', '30min.mp4', '45min.mp4']
+        Tim.fillfile(paths)
+        installtatio_neede_time = Tim.calculate_average()
+        tmp.jsonfile(installtatio_neede_time)
+        tmp.delete_tmp_file()
 
 
 
@@ -17,3 +21,4 @@ class Installation:
 
 if __name__ == "__main__":
     Installation.startup()
+        #0.1220757381170537
